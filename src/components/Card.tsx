39158d547a6bee1f045575ce6a9127d4ff723103
Russell Frankpitt <75209707+rustyff12@users.noreply.github.com>
2026-01-26 21:48:54 +0100

    Before added to h1

---
+++ src/components/Card.tsx
@@ -0,0 +1,12 @@
+import Image from "next/image";
+export default function Card() {
+  return (
+    <article className="card">
+      <h3>Snappy Process</h3>
+      <p>
+        Our application process can be completed in minutes, not hours.
+        Don&appos;t get stuck filling in tedious forms.
+      </p>
+    </article>
+  );
+}
